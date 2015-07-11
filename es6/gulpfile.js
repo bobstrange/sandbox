f16b98c691538492dab2bb935617fb7486211bb9
bob1983 <bob.1983.g@gmail.com>
2015-07-11 23:59:31 +0900

    Fixed gulp settings

--- es6/gulpfile.js
+++ es6/gulpfile.js
@@ -5,7 +5,7 @@ var ES6_PATH = './es6/';
 var COMPILED_FILE_PATH = './source/';
 
 gulp.task('babel', function() {
-  gulp.src(ES6_PATH + '*.es6');
+  gulp.src(ES6_PATH + '*.es6')
     .pipe(babel())
     .pipe(gulp.dest(COMPILED_FILE_PATH));
 });
