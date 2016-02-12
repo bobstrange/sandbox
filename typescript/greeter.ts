ea702a534a51401dbba00bcad4e0f352ca013705
bob1983 <bob.1983.g@gmail.com>
2016-02-12 17:28:57 +0900

    fix

--- typescript/greeter.ts
+++ typescript/greeter.ts
@@ -1,7 +1,7 @@
-function greeter(person) {
+function greeter(person: string) {
     return `Hello, $(person)`;
 }
 
 const user = "Jane User";
-
+const error = [1, 2, 3];
 document.body.innerHTML = greeter(user);
