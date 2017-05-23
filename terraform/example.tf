fcc273ea9c475dfaad370a5435b0d89e87655674
bob1983 <bob.1983.g@gmail.com>
2017-05-23 19:20:31 +0900

    provisioner sample

--- terraform/example.tf
+++ terraform/example.tf
@@ -7,14 +7,29 @@ resource "aws_instance" "example" {
   # ami           = "ami-afb09dc8"
   ami           = "ami-923d12f5"
   instance_type = "t2.micro"
+
+  # https://www.terraform.io/docs/provisioners/index.html
+  # - chef
+  # - connection
+  # - file
+  # - local-exec
+  # - remote-exec
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-}
+# resource "aws_eip" "ip" {
+#   instance = "${aws_instance.example.id}"
+# }
 
-resource "aws_instance" "example2" {
-  # ami           = "ami-afb09dc8"
-  ami           = "ami-923d12f5"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "example2" {
+#   ami           = "ami-923d12f5"
+#   instance_type = "t2.micro"
+# }
