d78b5d9c66149c779435fb095e5c2aa74834addc
bob1983 <bob.1983.g@gmail.com>
2020-01-25 22:39:14 +0900

    Tmp

--- effective_typescript/js/README.md
+++ effective_typescript/js/README.md
@@ -4,3 +4,43 @@
 
 Monitor for any changes in yoru node.js application and automatically restart
 the server.
+
+- Spread operators
+- private properties 
+    - `_` prefix
+    - (proposal) `#` prefix
+- Map
+
+## Map
+Objectは文字列のみしかキーにできないという制約がある。
+キーと値での組み合わせでデータを表現する`Map`がある。
+
+```js
+const data = new Map(
+  [
+    ['js', { name: 'JavaScript' }],
+    ['ts', { name: 'TypeScript' }],
+  ]
+)
+// Map自身もIteratable
+for (const [key, value] of data) {
+  console.log(`key: ${key} value: ${JSON.stringify(value)}`)
+}
+
+// Map.entries()はMapのDefaultのIterator
+for (const [key, value] of data.entries()) {
+  console.log(`key: ${key} value: ${JSON.stringify(value)}`)
+}
+
+// Map.keys()/Map.values()でキーのみ、値のみのIterator
+for (const key of data.keys()) {
+  console.log(`key: ${key}`)
+}
+for (const value of data.values()) {
+  console.log(`value: ${JSON.stringify(value)}`)
+}
+```
+
+Mapはどんな値もキーとして使うことができる。
+特に、`Symbol`は、UniqueでImmutableなので、キーとして適切。
+
--- effective_typescript/js/index.js
+++ effective_typescript/js/index.js
@@ -18,3 +18,62 @@ console.log(`Other Hat name: ${replaceProperties.name}, price: ${replaceProperti
 const { price, ...restProperties } = hat
 console.log(`Price: ${price} Rest properties ${JSON.stringify(restProperties)}`)
 
+/** Map */
+
+class Product {
+  constructor(name, price) {
+    this.name = name
+    this.price = price
+  }
+
+  toString() {
+    return `Name: ${this.name} Price: ${this.price}`
+  }
+}
+
+const dataStore = new Map()
+dataStore.set('hat', new Product('Hat', 1000))
+dataStore.set('boot', new Product('Boot', 2000))
+
+// keys()
+console.log(`Map.keys() ${dataStore.keys()}`) // Map.keys() [object Map Iterator]
+for (const key of dataStore.keys()) {
+  console.log(`key: ${key}`)
+}
+
+// values()
+console.log(`Map.values() ${dataStore.values()}`) // Map.values() [object Map Iterator]
+for (const value of dataStore.values()) {
+  console.log(`value: ${value}`)
+}
+
+// entries()
+console.log(`Map.entries() ${dataStore.entries()}`) // Map.entries() [object Map Iterator
+for (const entry of dataStore.entries()) {
+  console.log(`entry: ${entry}`)
+}
+// key-value pair
+// entry: hat,Name: Hat Price: 1000
+// entry: boot,Name: Boot Price: 2000
+
+for (const [key, value] of dataStore) {
+  console.log(`key: ${key} value: ${value}`)
+}
+const data = new Map(
+  [
+    ['js', { name: 'JavaScript' }],
+    ['ts', { name: 'TypeScript' }],
+  ]
+)
+for (const [key, value] of data) {
+  console.log(`key: ${key} value: ${JSON.stringify(value)}`)
+}
+
+// Map.keys()/Map.values()でキーのみ、値のみのIterator
+for (const key of data.keys()) {
+  console.log(`key: ${key}`)
+}
+for (const value of data.values()) {
+  console.log(`value: ${JSON.stringify(value)}`)
+}
+
