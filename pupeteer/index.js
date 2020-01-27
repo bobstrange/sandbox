94c85d3b7523c4078ff1e3d3fbb8f21a319756ea
bob1983 <bob.1983.g@gmail.com>
2020-01-27 21:50:05 +0900

    example

---
+++ pupeteer/index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  await page.waitFor(5000)
+
+  console.log('Dimensions:', dimensions);
+
+  await browser.close();
+})();
