c13fdcd3bd63939544adcf7eb014e655d44d374e
bob1983 <bob.1983.g@gmail.com>
2020-01-27 22:40:21 +0900

    Multiple element example

--- pupeteer/index.js
+++ pupeteer/index.js
@@ -2,20 +2,35 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
+  const page = await browser.newPage()
 
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
+  // await page.goto('https://news.ycombinator.com/news')
+  await page.goto('http://example.com')
 
-  await page.waitFor(5000)
+  // const data = await page.$eval('.hnname > a', el => el.innerText)
+  // const item = await page.$('p')
+  // const data = await (await item.getProperty('textContent')).jsonValue()
 
-  console.log('Dimensions:', dimensions);
+  // const item = await page.$('p')
+  // const data = await (await item.getProperty('textContent')).jsonValue()
+  const items = await page.$$('p')
+  // const data = items.map((item) => {
+  //   return await (await item.getProperty('textContent')).jsonValue()
+  // })
+  const data = await Promise.all(
+    items.map(async (item) => {
+      const contentRaw = await item.getProperty('textContent')
+      return await contentRaw.jsonValue()
+    })
+  )
 
-  await browser.close();
+  // const item = await page.$('table')
+  // const data = await (await item.getProperty('textContent')).jsonValue()
+  // const item = await page.$('table')
+  // const data = await (await item.getProperty('textContent')).jsonValue()
+
+  console.log(data)
+  console.log(data.length)
+  await page.waitFor(100000)
+  await browser.close()
 })();
