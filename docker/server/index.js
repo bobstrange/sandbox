b9b227d7445abc578244a109a8dd4cebe844ad3f
bob1983 <bob.1983.g@gmail.com>
2020-03-07 11:51:21 +0900

    Added docker compose

--- docker/server/index.js
+++ docker/server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
+const redisClient = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
+
+redisClient.set('visits', 0);
 
 app.get('/', (req, res) => {
-  res.send('Welcome');
+  redisClient.get('visits', (err, visits) => {
+    res.send(`Number of visits is ${visits}`);
+    redisClient.set('visits', parseInt(visits) + 1)
+  })
 })
 
 app.listen(8080, () => {
