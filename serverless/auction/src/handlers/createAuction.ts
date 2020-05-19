2780d6ee6a38ff6183cd8e01bbfb9ab12a8a644e
bob1983 <bob.1983.g@gmail.com>
2020-05-19 23:33:50 +0900

    Added dynamoDB integration

--- serverless/auction/src/handlers/createAuction.ts
+++ serverless/auction/src/handlers/createAuction.ts
@@ -1,19 +1,30 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { v4 as uuid } from 'uuid'
+import { DynamoDB } from 'aws-sdk'
+
+const dynamodb = new DynamoDB.DocumentClient()
 
 const createAuction: APIGatewayProxyHandler = async (event, _context) => {
   const { title } = JSON.parse(event.body)
   const now = new Date()
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString()
   }
+
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction
+  }).promise()
 
   return {
     statusCode: 201,
     body: JSON.stringify(auction)
   }
 }
+
 export const handler = createAuction
