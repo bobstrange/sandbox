8cc67ce627ac8fe7a9f0283a62f38f75d2a36e99
bob1983 <bob.1983.g@gmail.com>
2020-05-23 23:44:39 +0900

    Modify getAuctions not to use scan

--- serverless/auction/src/handlers/getAuctions.ts
+++ serverless/auction/src/handlers/getAuctions.ts
@@ -5,17 +5,27 @@ import "source-map-support/register";
 import { DynamoDB } from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
 
 const dynamodb = new DynamoDB.DocumentClient();
 
 const getAuctions: APIGatewayProxyHandler = async (_event, _context) => {
   let auctions
 
-  try {
-    const result = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME
-    }).promise()
+  const params: DocumentClient.QueryInput = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
+    ExpressionAttributeValues: {
+      ":status": "OPEN",
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
 
+  try {
+    const result = await dynamodb.query(params).promise()
     auctions = result.Items
   } catch (error) {
     console.error(error)
