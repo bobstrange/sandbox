de267ca7286b78bc422ebe4b901bd9b5f1af55d6
bob1983 <bob.1983.g@gmail.com>
2020-05-24 00:05:26 +0900

    Added status validator

--- serverless/auction/src/handlers/getAuctions.ts
+++ serverless/auction/src/handlers/getAuctions.ts
@@ -6,8 +6,11 @@ import { DynamoDB } from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import validator from '@middy/validator'
 
 const dynamodb = new DynamoDB.DocumentClient();
+
+
 
 const getAuctions: APIGatewayProxyHandler = async (event, _context) => {
   const { status } = event.queryStringParameters
@@ -40,4 +43,30 @@ const getAuctions: APIGatewayProxyHandler = async (event, _context) => {
   };
 };
 
+const getAuctionsSchema = {
+  properties: {
+    queryStringParameters: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'string',
+          enum: ['OPEN', 'CLOSED'],
+          default: 'OPEN'
+        }
+      }
+    }
+  },
+  required: [
+    'queryStringParameters'
+  ]
+}
+
 export const handler = commonMiddleware(getAuctions)
+  .use(
+    validator({
+      inputSchema: getAuctionsSchema,
+      ajvOptions: {
+        useDefaults: true
+      }
+    })
+  )
