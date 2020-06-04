77d70486261994a06f79eeb121ef3ceab9bdc6c0
bob1983 <bob.1983.g@gmail.com>
2020-06-05 01:02:18 +0900

    comment create impl

---
+++ microservice/posts-and-comments/client/src/CommentCreate.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(`http://localhost:8081/posts/${postId}/comments`, { content });
+
+    setContent('')
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
