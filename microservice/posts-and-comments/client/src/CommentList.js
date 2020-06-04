d2287642a77be2e90bcf66d13d522135cd5ea39e
bob1983 <bob.1983.g@gmail.com>
2020-06-05 01:23:00 +0900

    Comment list impl

---
+++ microservice/posts-and-comments/client/src/CommentList.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default ({ postId }) => {
+  const [comments, setComments] = useState([])
+
+  const fetchComments = async () => {
+    const response = await axios.get(`http://localhost:8081/posts/${postId}/comments`)
+    setComments(response.data)
+  }
+
+  useEffect(() => {
+    fetchComments()
+  }, [])
+
+  const renderedComments = comments.map(comment => {
+    return <li key={comment.id}>{comment.content}</li>
+  })
+  return <ul>
+    {renderedComments}
+  </ul>
+}
