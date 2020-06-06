5900c2912216ee7bf92b6acd1eb776647b174cbe
bob1983 <bob.1983.g@gmail.com>
2020-06-06 22:41:03 +0900

    emit PostCreated event

--- microservice/posts-and-comments/posts/index.js
+++ microservice/posts-and-comments/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -9,14 +10,19 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
+app.get('/posts', async (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
   posts[id] = { id, title }
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: { id, title }
+  })
 
   res.status(201).send(posts[id])
 })
