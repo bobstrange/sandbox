3251347b5c73b999eff9bad580b19891ea8e9532
bob1983 <bob.1983.g@gmail.com>
2020-06-09 23:04:55 +0900

    communicate each services

--- microservice/posts-and-comments/client/src/CommentCreate.js
+++ microservice/posts-and-comments/client/src/CommentCreate.js
@@ -10,7 +10,7 @@ export default ({ postId }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post(`http://comments-cluster-ip-srv:8081/posts/${postId}/comments`, { content });
+    await axios.post(`http://posts.com/posts/${postId}/comments`, { content });
 
     setContent('')
   };
--- microservice/posts-and-comments/posts/index.js
+++ microservice/posts-and-comments/posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', async (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
   posts[id] = { id, title }
@@ -33,5 +33,6 @@ app.post('/events', (req, res) => {
 })
 
 app.listen(8080, () => {
+  console.log('test')
   console.log('Listening on port 8080')
 })
