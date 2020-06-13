b0e58701dcfdb2d504f706a475600d7f22676181
bob1983 <bob.1983.g@gmail.com>
2020-06-13 23:19:49 +0900

    move logic to handler

--- microservice/booker/auth/src/index.ts
+++ microservice/booker/auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express'
 import { json } from 'body-parser'
+import { currentUserRouter } from './routes/current-user'
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there')
-})
+app.use(currentUserRouter)
 
 app.listen(8080, () => {
   console.log('Listening on port 8080')
