77a9340438e9fe26640ecc88f5da22ebeb8f11d8
bob1983 <bob.1983.g@gmail.com>
2020-06-13 23:24:30 +0900

    fix handler

--- microservice/booker/auth/src/index.ts
+++ microservice/booker/auth/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import { json } from 'body-parser'
+
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
 import { currentUserRouter } from './routes/current-user'
 
 const app = express()
 app.use(json())
 
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 app.use(currentUserRouter)
 
 app.listen(8080, () => {
--- microservice/booker/auth/src/routes/signin.ts
+++ microservice/booker/auth/src/routes/signin.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/signout", (req, res) => {
+router.get("/api/users/signin", (req, res) => {
   res.send("Hi there");
 });
 
-export { router as signoutRouter };
+export { router as signinRouter };
