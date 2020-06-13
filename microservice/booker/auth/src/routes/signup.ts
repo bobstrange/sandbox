04cae157f72c73122fc18ef2d5c9d0dcadf23b80
bob1983 <bob.1983.g@gmail.com>
2020-06-13 23:58:13 +0900

    move validations

--- microservice/booker/auth/src/routes/signup.ts
+++ microservice/booker/auth/src/routes/signup.ts
@@ -3,12 +3,12 @@ import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post(
-'/api/users/signup', [
+const validations = [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-],
-(req: Request, res: Response) => {
+]
+
+router.post('/api/users/signup', validations, (req: Request, res: Response) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
