8df17d7cc88868fa170d9fe1822f6d9c2e6a4877
bob1983 <bob.1983.g@gmail.com>
2020-06-15 23:13:28 +0900

    handle existing email

--- microservice/booker/auth/src/routes/signup.ts
+++ microservice/booker/auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
+import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router()
 
@@ -22,8 +23,7 @@ router.post('/api/users/signup', validations, async (req: Request, res: Response
   const existingUser = await User.findOne({ email })
 
   if (existingUser) {
-    console.log('Email in use')
-    return res.send({})
+    throw new BadRequestError('Eamil in use')
   }
 
   const user = User.build({ email, password })
