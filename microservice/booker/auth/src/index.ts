2369b899461e9a8df9b988f0aabb877833ed5393
bob1983 <bob.1983.g@gmail.com>
2020-06-16 23:22:45 +0900

    Added cookie-session

--- microservice/booker/auth/src/index.ts
+++ microservice/booker/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -12,7 +13,12 @@ import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
 
 app.use(signinRouter)
 app.use(signoutRouter)
