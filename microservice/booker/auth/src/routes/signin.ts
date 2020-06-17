4dabb424cceba138796c8857abde15ad7245948b
bob1983 <bob.1983.g@gmail.com>
2020-06-17 23:50:23 +0900

    Added validateRequest middleware

--- microservice/booker/auth/src/routes/signin.ts
+++ microservice/booker/auth/src/routes/signin.ts
@@ -1,9 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.get("/api/users/signin", (req, res) => {
-  res.send("Hi there");
-});
+const validations = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('You must supply a password')
+]
+
+router.post(
+  '/api/users/signin',
+  validations,
+  validateRequest,
+  (req: Request, res: Response) => {
+  }
+);
 
 export { router as signinRouter };
--- microservice/booker/auth/src/routes/signup.ts
+++ microservice/booker/auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import jwt from 'jsonwebtoken'
 
+import { validateRequest } from '../middlewares/validate-request'
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
@@ -9,42 +10,46 @@ import { BadRequestError } from '../errors/bad-request-error'
 const router = express.Router()
 
 const validations = [
-  body('email').isEmail().withMessage('Email must be valid'),
-  body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
 ]
 
-router.post('/api/users/signup', validations, async (req: Request, res: Response) => {
-  const errors = validationResult(req)
+router.post(
+  '/api/users/signup',
+  validations,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      throw new BadRequestError('Eamil in use')
+    }
+
+    const user = User.build({ email, password })
+    await user.save()
+
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    )
+
+    // @ts-ignore
+    req.session = {
+      jwt: userJwt
+    }
+
+    res.status(201).send(user)
   }
-
-  const { email, password } = req.body
-
-  const existingUser = await User.findOne({ email })
-
-  if (existingUser) {
-    throw new BadRequestError('Eamil in use')
-  }
-
-  const user = User.build({ email, password })
-  await user.save()
-
-  const userJwt = jwt.sign(
-    {
-      id: user.id,
-      email: user.email
-    },
-    process.env.JWT_KEY!
-  )
-
-  // @ts-ignore
-  req.session = {
-    jwt: userJwt
-  }
-
-  res.status(201).send(user)
-})
+)
 
 export { router as signupRouter }
