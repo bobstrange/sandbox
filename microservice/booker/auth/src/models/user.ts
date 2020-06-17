09a65d9558d31a736cc230b47a31a17b5789dc15
bob1983 <bob.1983.g@gmail.com>
2020-06-17 23:58:48 +0900

    rename module name

--- microservice/booker/auth/src/models/user.ts
+++ microservice/booker/auth/src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import { Password } from '../services/password'
+import { PasswordManager } from '../services/password-manager'
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(done) {
   if (this.isModified('password')) {
-    const hashed = await Password.toHash(this.get('password'))
+    const hashed = await PasswordManager.toHash(this.get('password'))
     this.set('password', hashed)
   }
   done()
