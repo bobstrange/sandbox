f5572db14d3389e673f0349bee046c373db427c5
bob1983 <bob.1983.g@gmail.com>
2020-06-19 00:24:46 +0900

    Added current user test

---
+++ microservice/booker/auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+describe('currentUserRouter', () => {
+  it('responds with details about the current user', async () => {
+    const signupResponse = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .expect(201)
+    const cookie = signupResponse.get('Set-Cookie')
+
+    const response = await request(app)
+      .post('/api/users/currentuser')
+      .set('Cookie', cookie)
+      .send()
+      .expect(200)
+
+    expect(response.body.currentUser.email).toEqual('test@test.com')
+  })
+})
