2f2386964f1a277a7b9511a4fc55e75025d49137
bob1983 <bob.1983.g@gmail.com>
2020-06-18 21:31:40 +0900

    Added currentUser middleware

--- microservice/booker/auth/src/routes/current-user.ts
+++ microservice/booker/auth/src/routes/current-user.ts
@@ -1,22 +1,11 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.post('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null })
-  }
-
-  try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    )
-    res.send({ currentUser: payload })
-  } catch (error) {
-    res.send({ currentUser: null })
-  }
+router.post('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
