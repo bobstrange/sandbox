7993b04bcfba8c4376e70362643d905eebbbcaee
bob1983 <bob.1983.g@gmail.com>
2020-06-19 00:01:37 +0900

    Make cookie test pass

--- microservice/booker/auth/src/routes/__test__/signup.test.ts
+++ microservice/booker/auth/src/routes/__test__/signup.test.ts
@@ -46,4 +46,33 @@ describe('signupRouter', () => {
       })
       .expect(400)
   })
+
+  it('disallows duplicated email', async () => {
+    await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .expect(201)
+    await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .expect(400)
+  })
+
+  it('sets a cookie after successful signup', async () => {
+    const response = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .expect(201)
+    expect(response.get('Set-Cookie')).toBeDefined()
+
+  })
 })
