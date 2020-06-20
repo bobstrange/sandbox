6b37d478227cd1414082f56498e5608d97150875
bob1983 <bob.1983.g@gmail.com>
2020-06-20 22:37:45 +0900

    Fix currentuser

--- microservice/booker/auth/src/routes/__test__/current-user.test.ts
+++ microservice/booker/auth/src/routes/__test__/current-user.test.ts
@@ -6,11 +6,19 @@ describe('currentUserRouter', () => {
     const cookie = await global.signin()
 
     const response = await request(app)
-      .post('/api/users/currentuser')
+      .get('/api/users/currentuser')
       .set('Cookie', cookie)
       .send()
       .expect(200)
 
     expect(response.body.currentUser.email).toEqual('test@test.com')
+  })
+
+  it('responds with null if not authenticated', async () => {
+    const response = await request(app)
+      .get('/api/users/currentuser')
+      .send()
+      .expect(200)
+    expect(response.body.currentUser).toEqual(null)
   })
 })
--- microservice/booker/auth/src/routes/current-user.ts
+++ microservice/booker/auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.post('/api/users/currentuser', currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null })
 })
 
