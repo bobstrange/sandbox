fee82df95d3b476ff0bf34741e1e8070f96da30e
bob1983 <bob.1983.g@gmail.com>
2020-06-26 22:55:52 +0900

    Added signin

--- microservice/booker/client/pages/index.js
+++ microservice/booker/client/pages/index.js
@@ -1,12 +1,12 @@
 import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser)
-  return <h1>Landing page</h1>
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are not sign in</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get('/api/users/currentuser')
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser')
   return data
 }
 
