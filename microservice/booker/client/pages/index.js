fcb7ace17d0df14180dcce868145ba92b36c39e8
bob1983 <bob.1983.g@gmail.com>
2020-06-26 22:47:08 +0900

    Move axios related thing to api client

--- microservice/booker/client/pages/index.js
+++ microservice/booker/client/pages/index.js
@@ -1,25 +1,13 @@
-import axios from 'axios'
+import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser)
   return <h1>Landing page</h1>
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // on the server
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    )
-    return data
-  } else {
-    // on the browser
-    const { data } = await axios.get('/api/users/currentuser')
-    return data
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser')
+  return data
 }
 
 export default LandingPage
