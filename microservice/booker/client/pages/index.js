f1b627ca8c025190368c81a80be230ccc5064947
bob1983 <bob.1983.g@gmail.com>
2020-06-26 22:35:59 +0900

    Make ssr working

--- microservice/booker/client/pages/index.js
+++ microservice/booker/client/pages/index.js
@@ -6,8 +6,22 @@ const LandingPage = ({ currentUser }) => {
 }
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser')
-  return response.data
+  if (typeof window === 'undefined') {
+    // on the server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'booker.dev',
+        },
+      }
+    )
+    return data
+  } else {
+    // on the browser
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
 }
 
 export default LandingPage
