1ed698ce2d5a717f06d62ba8cbfeb799a72a63d8
bob1983 <bob.1983.g@gmail.com>
2020-06-26 23:25:14 +0900

    Added header

--- microservice/booker/client/pages/_app.js
+++ microservice/booker/client/pages/_app.js
@@ -1,5 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+import buildClient from '../api/build-client'
+import Header from '../components/header'
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+  const { ctx, Component } = appContext
+  const client = buildClient(ctx)
+  const { data } = await client.get('/api/users/currentuser')
+
+  let pageProps = {}
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return {
+    pageProps,
+    currentUser: data.currentUser,
+  }
+}
+export default AppComponent
