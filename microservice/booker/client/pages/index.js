99722500d2f30f229d81b6839c57037d51979339
bob1983 <bob.1983.g@gmail.com>
2020-06-26 22:39:01 +0900

    Pass cookie information while SSR

--- microservice/booker/client/pages/index.js
+++ microservice/booker/client/pages/index.js
@@ -5,15 +5,13 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing page</h1>
 }
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // on the server
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'booker.dev',
-        },
+        headers: req.headers,
       }
     )
     return data
