cb728d762be4df1700a6e3a1c94375acdd6726fd
bob1983 <bob.1983.g@gmail.com>
2020-06-28 23:55:39 +0900

    Make price and title validation test

--- microservice/booker/tickets/src/routes/__test__/new.test.ts
+++ microservice/booker/tickets/src/routes/__test__/new.test.ts
@@ -20,7 +20,31 @@ describe('createTicketRouter', () => {
     expect(response.status).not.toEqual(401)
   })
 
-  it('returns an error if an invalid title is provided', async () => {})
-  it('returns an error if an invalid price is provided', async () => {})
+  it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', global.signin())
+      .send({ title: '', price: 10 })
+      .expect(400)
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', global.signin())
+      .send({ price: 10 })
+      .expect(400)
+  })
+
+  it('returns an error if an invalid price is provided', async () => {
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', global.signin())
+      .send({ title: 'test', price: -10 })
+      .expect(400)
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', global.signin())
+      .send({ title: 'test' })
+      .expect(400)
+  })
+
   it('creates a ticket with valid inputs', async () => {})
 })
