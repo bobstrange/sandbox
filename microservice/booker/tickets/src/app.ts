a9a1800349980c36b06620340ea5a2edb9ef6d03
bob1983 <bob.1983.g@gmail.com>
2020-06-29 00:52:27 +0900

    Added show ticket route

--- microservice/booker/tickets/src/app.ts
+++ microservice/booker/tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@bsbooker/common'
 
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
 
 const app = express()
 app.set('trust proxy', true)
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser)
 
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 
 app.all('*', async () => {
   throw new NotFoundError()
--- microservice/booker/tickets/src/routes/__test__/show.test.ts
+++ microservice/booker/tickets/src/routes/__test__/show.test.ts
@@ -1,10 +1,12 @@
 import request from 'supertest'
+import mongoose from 'mongoose'
 import { app } from '../../app'
-import { Ticket } from '../../models/ticket'
 
-describe('showRoute', () => {
+describe('showTicketRouter', () => {
   it('returns a 404 if the ticket is not found', async () => {
-    await request(app).post('/api/tickets/dfkjadkjf').send().expect(404)
+    const id = new mongoose.Types.ObjectId().toHexString()
+
+    await request(app).post(`/api/tickets/${id}`).send().expect(404)
   })
 
   it('returns a ticket if the ticket is found', async () => {
