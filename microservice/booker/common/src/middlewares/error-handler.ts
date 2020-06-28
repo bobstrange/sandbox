c32856b1fe849412b97cd98fb6932be28bde0a4e
bob1983 <bob.1983.g@gmail.com>
2020-06-29 00:46:24 +0900

    Added error log

--- microservice/booker/common/src/middlewares/error-handler.ts
+++ microservice/booker/common/src/middlewares/error-handler.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import { CustomError } from "../errors/custom-error";
+import { Request, Response, NextFunction } from 'express'
+import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (
   err: Error,
@@ -10,10 +10,12 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({
       errors: err.serializeErrors(),
-    });
+    })
   }
 
+  console.error(err)
+
   res.status(400).send({
-    errors: [{ message: "Something went wrong" }],
-  });
-};
+    errors: [{ message: 'Something went wrong' }],
+  })
+}
