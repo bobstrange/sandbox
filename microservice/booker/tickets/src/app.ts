90c2ea3643b36da17d6b234ad21544e57510537a
bob1983 <bob.1983.g@gmail.com>
2020-06-29 23:31:26 +0900

    Added indexRouteHandler

--- microservice/booker/tickets/src/app.ts
+++ microservice/booker/tickets/src/app.ts
@@ -6,6 +6,7 @@ import { errorHandler, NotFoundError, currentUser } from '@bsbooker/common'
 
 import { createTicketRouter } from './routes/new'
 import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes'
 
 const app = express()
 app.set('trust proxy', true)
@@ -20,6 +21,7 @@ app.use(currentUser)
 
 app.use(createTicketRouter)
 app.use(showTicketRouter)
+app.use(indexTicketRouter)
 
 app.all('*', async () => {
   throw new NotFoundError()
