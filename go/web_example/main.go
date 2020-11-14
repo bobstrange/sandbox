b88d1ca04fd7ad48d458e4ea757e9bbe73a8a675
bobstrange <bob.1983.g@gmail.com>
2020-11-15 00:01:30 +0900

    added login example

--- go/web_example/main.go
+++ go/web_example/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"text/template"
+	"time"
 )
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
@@ -19,8 +24,33 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello server!")
 }
 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			// Verify token
+			fmt.Println("token:", token)
+		} else {
+			// Error if token does not exist
+		}
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", loginHandler)
 	err := http.ListenAndServe(":4000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
