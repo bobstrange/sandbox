7f4f78a87d684dcc47d10a36f6bdbc57cd22f8a3
bobstrange <bob.1983.g@gmail.com>
2020-11-29 23:33:01 +0900

    closing-channels.go

---
+++ go/go_by_example/closing-channels/closing-channels.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			// more は、jobs が close されている場合には false になる
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+
+	close(jobs)
+	fmt.Println("sent all jobs.")
+	<-done
+}
