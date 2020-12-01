0c17633a48cfffe93f585a031de79f01a08f8638
bobstrange <bob.1983.g@gmail.com>
2020-12-01 18:13:25 +0900

    connect to mongodb

---
+++ go/use-mongo/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	// "fmt"
+	"log"
+	"time"
+
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:37017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+}
