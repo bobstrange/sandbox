987fd7fc05498426e77c006bf98011e31dd86bce
bobstrange <bob.1983.g@gmail.com>
2020-12-16 21:29:59 +0900

    connect to host mongodb

--- sam-go/sns/main.go
+++ sam-go/sns/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
+	"context"
 	"log"
-	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
@@ -24,34 +25,34 @@ func handler(event events.SNSEvent) error {
 func main() {
 	log.Println("Debug: start main()")
 
-	url := "http://172.17.0.1:8000"
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal("Failed to get: ", err)
-	}
-	defer resp.Body.Close()
-
-	buf, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Failed to read: ", err)
-	}
-	log.Println(string(buf))
-
-	// client, err := mongo.NewClient(
-	// 	options.Client().ApplyURI("mongodb://user:password@host.docker.internal:48017"),
-	// )
-
+	// url := "http://172.17.0.1:8000"
+	// resp, err := http.Get(url)
 	// if err != nil {
-	// 	log.Fatal("Fail to create client")
+	// 	log.Fatal("Failed to get: ", err)
 	// }
+	// defer resp.Body.Close()
 
-	// if err := client.Connect(context.TODO()); err != nil {
-	// 	log.Fatal("Fail to connect")
+	// buf, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal("Failed to read: ", err)
 	// }
+	// log.Println(string(buf))
 
-	// if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-	// 	log.Fatal("Fail to ping")
-	// }
+	client, err := mongo.NewClient(
+		options.Client().ApplyURI("mongodb://user:password@172.17.0.1:47017"),
+	)
+
+	if err != nil {
+		log.Fatal("Fail to create client")
+	}
+
+	if err := client.Connect(context.TODO()); err != nil {
+		log.Fatal("Fail to connect")
+	}
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal("Fail to ping")
+	}
 
 	log.Println("Debug: lambda.Start()")
 	lambda.Start(handler)
