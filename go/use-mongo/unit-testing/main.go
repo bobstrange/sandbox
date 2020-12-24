5266b141db6f745c6192ec420676dcd991f91bb5
bobstrange <bob.1983.g@gmail.com>
2020-12-24 17:10:55 +0900

    feat: initialized mongodb connection

---
+++ go/use-mongo/unit-testing/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
+	opts := options.Client().ApplyURI("mongodb://localhost:37017")
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection = client.Database("tasker").Collection("tasks")
+}
