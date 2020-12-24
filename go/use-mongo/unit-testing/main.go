fce7db9390c3d29ddd91817a226086b21050d40a
bobstrange <bob.1983.g@gmail.com>
2020-12-24 17:36:52 +0900

    feat: list tasks

--- go/use-mongo/unit-testing/main.go
+++ go/use-mongo/unit-testing/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/urfave/cli/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/gookit/color.v1"
 )
 
 var collection *mongo.Collection
@@ -43,6 +46,50 @@ func createTask(task *Task) error {
 	return err
 }
 
+func getAll() ([]*Task, error) {
+	filter := bson.D{{}}
+	return filterTasks(filter)
+}
+
+func filterTasks(filter interface{}) ([]*Task, error) {
+	var tasks []*Task
+
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return tasks, err
+	}
+
+	for cur.Next(ctx) {
+		var t Task
+		if err := cur.Decode(&t); err != nil {
+			return tasks, err
+		}
+		tasks = append(tasks, &t)
+	}
+
+	if err := cur.Err(); err != nil {
+		return tasks, err
+	}
+
+	cur.Close(ctx)
+
+	if len(tasks) == 0 {
+		return tasks, mongo.ErrNoDocuments
+	}
+
+	return tasks, nil
+}
+
+func printTasks(tasks []*Task) {
+	for i, t := range tasks {
+		if t.Completed {
+			color.Green.Printf("%d: %s\n", i+1, t.Text)
+		} else {
+			color.Yellow.Printf("%d: %s\n", i+1, t.Text)
+		}
+	}
+}
+
 func addCommand() *cli.Command {
 	return &cli.Command{
 		Name:    "add",
@@ -65,12 +112,34 @@ func addCommand() *cli.Command {
 		},
 	}
 }
+
+func allCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "all",
+		Aliases: []string{"l"},
+		Usage:   "list all tasks",
+		Action: func(c *cli.Context) error {
+			tasks, err := getAll()
+			if err != nil {
+				if err == mongo.ErrNoDocuments {
+					fmt.Print("Nothing to see here.\nRun `add task` to add a task")
+					return nil
+				}
+				return err
+			}
+			printTasks(tasks)
+			return nil
+		},
+	}
+}
+
 func main() {
 	app := &cli.App{
 		Name:  "tasker",
 		Usage: "A simple CLI program to manage your tasks",
 		Commands: []*cli.Command{
 			addCommand(),
+			allCommand(),
 		},
 	}
 
