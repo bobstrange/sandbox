83906bd3fee3fe7b8f27781ff5754f1b4800f7df
bobstrange <bob.1983.g@gmail.com>
2020-12-24 17:21:24 +0900

    feat: insert a task function

--- go/use-mongo/unit-testing/main.go
+++ go/use-mongo/unit-testing/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,7 +17,7 @@ var collection *mongo.Collection
 var ctx = context.TODO()
 
 func init() {
-	opts := options.Client().ApplyURI("mongodb://localhost:37017")
+	opts := options.Client().ApplyURI("mongodb://user:password@localhost:37017")
 	client, err := mongo.Connect(ctx, opts)
 	if err != nil {
 		log.Fatal(err)
@@ -26,11 +29,49 @@ func init() {
 	collection = client.Database("tasker").Collection("tasks")
 }
 
+// Task task entity
+type Task struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	Text      string             `bson:"text"`
+	Completed bool               `bson:"completed"`
+}
+
+func createTask(task *Task) error {
+	_, err := collection.InsertOne(ctx, task)
+	return err
+}
+
+func addCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "add",
+		Aliases: []string{"a"},
+		Usage:   "add a task to the list",
+		Action: func(c *cli.Context) error {
+			text := c.Args().First()
+			if text == "" {
+				return errors.New("Cannot add an empty task")
+			}
+
+			task := &Task{
+				ID:        primitive.NewObjectID(),
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				Text:      text,
+				Completed: false,
+			}
+			return createTask(task)
+		},
+	}
+}
 func main() {
 	app := &cli.App{
-		Name:     "tasker",
-		Usage:    "A simple CLI program to manage your tasks",
-		Commands: []*cli.Command{},
+		Name:  "tasker",
+		Usage: "A simple CLI program to manage your tasks",
+		Commands: []*cli.Command{
+			addCommand(),
+		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
