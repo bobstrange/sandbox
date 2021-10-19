a08b071259bcd06cce01ac6803cb0e3bf54c0743
bobstrange <bob.1983.g@gmail.com>
2021-10-19 23:45:05 +0900

    feat: q007 done

--- shell-script/shell_one_liner_160/q007.sh
+++ shell-script/shell_one_liner_160/q007.sh
@@ -5,6 +5,9 @@ tmp="$(pwd)"
 
 cd ./shellgei160/qdata/7
 
-cat kakeibo.txt
-cat kakeibo.txt | awk '{print $1,$2,$3 * 1.1}'
+cat kakeibo.txt | \
+  awk '{ tax = ($1 < "20191001" || $2 ~ "^*") ? 1.08 : 1.1 ; print $0, tax }' | \
+  awk '{ print int($3 * $4) }' | \
+  awk '{ total += $1 }END{ print total }'
+
 cd "${tmp}"
