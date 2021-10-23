04199444c0881feabd3333f50dd98ba284dde164
bobstrange <bob.1983.g@gmail.com>
2021-10-23 23:57:44 +0900

    remove unused

--- ruby/github/security_alerts.rb
+++ ruby/github/security_alerts.rb
@@ -7,48 +7,5 @@ Dotenv.load
 
 client = Octokit::Client.new(access_token: ENV["TOKEN"])
 
-# query = <<~GRAPHQL
-# query {
-# repository(owner:"#{ENV["OWNER"]}", name:"#{ENV["REPO"]}") {
-# id
-# name
-# description
-# vulnerabilityAlerts(first: 100) {
-# totalCount
-# nodes {
-# createdAt
-# dismisser {
-# id
-# name
-# }
-# dismissedAt
-# dismissReason
-# repository {
-# id
-# nameWithOwner
-# }
-# id
-# securityVulnerability {
-# advisory {
-# id
-# ghsaId
-# description
-# summary
-# severity
-# updatedAt
-# }
-# severity
-# }
-# vulnerableManifestFilename
-# vulnerableManifestPath
-# vulnerableRequirements
-# }
-# }
-# }
-# }
-# GRAPHQL
-# result = client.post("/graphql", { query: query }.to_json).to_hash
-# result.dig(:data, :repository, :vulnerabilityAlerts, :nodes)
-
 result = client.get("/repos/#{ENV { "OWNER" }}/#{ENV["REPO"]}/secret-scanning/alerts")
 binding.pry
