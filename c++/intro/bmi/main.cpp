1484ecec7b8dde317b291cf12d403836eaefecf7
bobstrange <bob.1983.g@gmail.com>
2021-11-13 19:59:48 +0900

    feat: add status to bmi

--- c++/intro/bmi/main.cpp
+++ c++/intro/bmi/main.cpp
@@ -4,9 +4,25 @@ int main() {
   double h = 1.63;
   double m = 72.0;
 
-  auto bmi = [](double height, double mass) {
+  auto calcbmi = [](double height, double mass) {
     return mass / (height * height);
   };
 
-  std::cout << "BMI is " << bmi(h, m) << "\n"s;
+  double bmi = calcbmi(h, m);
+
+  std::cout << "BMI is " << bmi << "\n"s;
+
+  auto status = [](double bmi) {
+    if (bmi < 18.5) {
+      return "やせすぎ"s;
+    } else if (bmi < 25.0) {
+      return "普通"s;
+    } else if (bmi < 30.0) {
+      return "太り気味"s;
+    } else {
+      return "肥満"s;
+    }
+  };
+
+  std::cout << "Status is " << status(bmi) << "\n"s;
 }
