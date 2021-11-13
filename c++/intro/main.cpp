3d05f6763fecfd94dbef83b654ff11e76d1a6743
bobstrange <bob.1983.g@gmail.com>
2021-11-13 19:34:18 +0900

    chore: include "all.h" into main to suppress errors

--- c++/intro/main.cpp
+++ c++/intro/main.cpp
@@ -1,3 +1,5 @@
+#include "all.h"
+
 int main() {
   std::cout << "Integer: " << 42 << "\n"s;
 
@@ -5,4 +7,11 @@ int main() {
   auto answer = 42;
   std::cout << answer << "\n"s;
 
+  // 関数
+  auto print = [](auto x) {
+    std::cout << x << "\n"s;
+  };
+
+  print(100);
+  print("something"s);
 }
