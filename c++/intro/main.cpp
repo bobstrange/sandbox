1f75608ae55d5072dba7a0907c413911396993f2
bobstrange <bob.1983.g@gmail.com>
2021-11-13 19:38:47 +0900

    chore: function

--- c++/intro/main.cpp
+++ c++/intro/main.cpp
@@ -1,5 +1,11 @@
 #include "all.h"
 
+
+// 関数の場合は引数は auto にはできない
+auto plus (int x, int y) {
+  return x + y;
+}
+
 int main() {
   std::cout << "Integer: " << 42 << "\n"s;
 
@@ -7,10 +13,8 @@ int main() {
   auto answer = 42;
   std::cout << answer << "\n"s;
 
-  // 関数
-  auto print = [](auto x) {
-    std::cout << x << "\n"s;
-  };
+  // 関数 (ラムダ式の場合は引数も auto にできる)
+  auto print = [](auto x) { std::cout << x << "\n"s; };
 
   print(100);
   print("something"s);
