79b7d6e64b029d4e3a95eb946b12decb78b7660b
bobstrange <bob.1983.g@gmail.com>
2021-11-18 23:06:20 +0900

    chore: selection sort

--- c++/intro/dynamically_allocate_memory/main.cpp
+++ c++/intro/dynamically_allocate_memory/main.cpp
@@ -54,12 +54,20 @@ int main() {
   std::vector<int> v = {8, 3, 7, 4, 2, 9, 3};
   std::size_t size = v.size();
 
-  std::size_t min = 0;
+  for (std::size_t head = 0; head < size; head++) {
+    std::size_t min = head;
 
-  for(std::size_t index = 0; index < size; index++) {
-    if (v.at(index) < v.at(min)) {
-      min = index;
+    for (std::size_t index = head + 1; index < size; index++) {
+      if (v.at(index) < v.at(min)) {
+        min = index;
+      }
     }
+    auto tmp = v.at(head);
+    v.at(head) = v.at(min);
+    v.at(min) = tmp;
   }
-  std::cout << "min value index is :"s << min << " value is: "s << v.at(min) << "\n"s;
+
+  for (std::size_t index = 0; index < size; index++) {
+    std::cout << v.at(index) << "\n"s;
+  }
 }
