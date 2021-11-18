b32b4d9df148d472bc199b187ec5db337c569d19
bobstrange <bob.1983.g@gmail.com>
2021-11-18 23:23:52 +0900

    chore: number

--- c++/intro/main.cpp
+++ c++/intro/main.cpp
@@ -6,8 +6,13 @@ auto plus (int x, int y) {
   return x + y;
 }
 
-int main() {
-  std::cout << "Integer: " << 42 << "\n"s;
+void std_err() {
+  std::cerr << "標準エラー出力なので > /dev/null しても表示されるよ\n"s;
+  std::cerr << "標準エラー出力なので 2> じゃないとリダイレクト出来ないよ\n"s;
+}
+
+void vars_and_funcs() {
+  std::cout << "Integer: "s << 42 << "\n"s;
 
   // 変数
   auto answer = 42;
@@ -15,12 +20,39 @@ int main() {
 
   // 関数 (ラムダ式の場合は引数も auto にできる)
   auto print = [](auto x) { std::cout << x << "\n"s; };
-
   print(100);
   print("something"s);
-
-  // 標準エラー出力は、std::
-
-  std::cerr << "標準エラー出力なので > /dev/null しても表示されるよ\n"s;
-  std::cerr << "標準エラー出力なので 2> じゃないとリダイレクト出来ないよ\n"s;
 }
+
+void number() {
+  int decimal = 123;
+
+  // 0 をつけると 8 進数になる
+  int octal = 0123;
+
+  // 2 進数は 0b or 0B で始まる
+  int binary = 0b1010;
+
+  // 16 進数は 0x で始まる
+  int hexadecimal = 0x123;
+
+  std::cout << "decimal: "s << decimal << "\n"s;
+  std::cout << "binary: "s << binary << "\n"s;
+  std::cout << "octal: "s << octal << "\n"s;
+  std::cout << "hexadecimal: "s << hexadecimal << "\n"s;
+  // 出力は全部 10 進数になる
+  // decimal: 123
+  // binary: 10
+  // octal: 83
+  // hexadecimal: 291
+
+  // ruby の　_ みたいな感じで数値を区切れる
+  int a = 1000'000;
+}
+
+int main() {
+  vars_and_funcs();
+  std_err();
+  number();
+}
+
