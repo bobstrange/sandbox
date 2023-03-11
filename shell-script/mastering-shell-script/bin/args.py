20ec2be085a1e0a416a1fcc193bc3b5f7491caef
bobstrange <bob.1983.g@gmail.com>
2023-03-11 23:14:51 +0900

    chore: args.py input

--- shell-script/mastering-shell-script/bin/args.py
+++ shell-script/mastering-shell-script/bin/args.py
@@ -3,7 +3,11 @@
 import sys
 
 count = len(sys.argv)
-if count > 1:
-    print(f"Arguments suppried: {len(sys.argv)}")
-    print(f"Hello {sys.argv[1]}")
+
+if count == 1:
+  name = input("Enter a name: ")
+else:
+  name = sys.argv[1]
+
+print(f"Hello {name}")
 print(f"Exiting {sys.argv[0]}")
