d442f7098cf7a78f1766f54ec989c0050cd90170
bobstrange <bob.1983.g@gmail.com>
2023-05-01 00:08:05 +0900

    chore: pattern3

--- ruby/polished-ruby-programming-memo/src/001_in_memory_database.rb
+++ ruby/polished-ruby-programming-memo/src/001_in_memory_database.rb
@@ -10,40 +10,61 @@ end
 # Pattern 1
 # store album artists and album track artists separately
 # Memory usage: not good Search speed: good
-album_artists = {}
-album_track_artists = {}
-album_infos.each do |album, track, artist|
-  (album_artists[album] ||= []) << artist
-  (album_track_artists[[album, track]] ||= []) << artist
-end
-album_artists.each_value(&:uniq!)
+def pattern1(album_infos)
+  album_artists = {}
+  album_track_artists = {}
+  album_infos.each do |album, track, artist|
+    (album_artists[album] ||= []) << artist
+    (album_track_artists[[album, track]] ||= []) << artist
+  end
+  album_artists.each_value(&:uniq!)
 
-lookup = lambda do |album, track = nil|
-  if track
-    album_track_artists[[album, track]]
-  else
-    album_artists[album]
+  lookup = lambda do |album, track = nil|
+    if track
+      album_track_artists[[album, track]]
+    else
+      album_artists[album]
+    end
   end
 end
-
-puts album_artists.size
 
 # Pattern 2
 # store album artists and album track artists together
 # Memory usage: good
 # Search speed: not good. If you want to search by only album name, we have to
 # generate artist list for all tracks in the album.
-albums = {}
-album_infos.each do |album, track, artist|
-  ((albums[album] ||= {})[track] ||= []) << artist
+
+def pattern2(album_infos)
+  albums = {}
+  album_infos.each do |album, track, artist|
+    ((albums[album] ||= {})[track] ||= []) << artist
+  end
+
+  lookup = lambda do |album, track = nil|
+    if track
+      albums.dig(album, track)
+    else
+      albums[album].map do |_, artist|
+        artist
+      end.uniq
+    end
+  end
 end
 
-lookup_album = lambda do |album, track = nil|
-  if track
+# Pattern 3
+def pattern3(album_infos)
+  albums = {}
+  album_infos.each do |album, track, artist|
+    album_array = albums[album] ||= [[]]
+    album_array[0] << artist
+    (album_array[track] ||= []) << artist
+  end
+
+  albums.each_value do |array|
+    array[0].uniq!
+  end
+
+  lookup = lambda do |album, track = 0|
     albums.dig(album, track)
-  else
-    albums[album].map do |_, artist|
-      artist
-    end.uniq
   end
 end
