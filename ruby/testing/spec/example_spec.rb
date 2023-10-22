8c872495dee8295d0975b413e1b63bb52240637d
bobstrange <bob.1983.g@gmail.com>
2023-10-22 23:39:05 +0900

    chore: add bad example

--- ruby/testing/spec/example_spec.rb
+++ ruby/testing/spec/example_spec.rb
@@ -4,6 +4,10 @@ end
 
 describe('Using not appropriate matcher') do
   it do
+    expect(list).to eq([1, 2, 3])
+  end
+
+  it do
     expect(list.include?(1)).to be_truthy
     expect(list.include?(2)).to be_truthy
     expect(list.include?(3)).to be_truthy
